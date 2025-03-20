9288d4c02b89f1258f3534d2737d69051f464e2d
cream <837800910@qq.com>
2025-03-21 02:26:12 +0800

    server init

---
+++ sparkle-server/src/main/java/com/cream/sparkle/SparkleServerApplication.java
@@ -0,0 +1,13 @@
+package com.cream.sparkle;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SparkleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SparkleServerApplication.class, args);
+    }
+
+}
---
+++ sparkle-server/src/test/java/com/cream/sparkle/SparkleServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.cream.sparkle;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SparkleServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
