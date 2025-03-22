2cd22b7404d24110ef5b636959c521c6c28595c7
cream <837800910@qq.com>
2025-03-22 21:56:16 +0800

    icon...

--- sparkle-cli/electron/main.ts
+++ sparkle-cli/electron/main.ts
@@ -14,7 +14,7 @@ function createWin() {
     height: 600,
     show: false,
     autoHideMenuBar: true,
-    icon: path.join(__dirname, "../public/fire.ico"),
+    icon: path.join(__dirname, "../public/fire.png"),
     webPreferences: {
       preload: path.join(__dirname, "./preload.js"),
       sandbox: false
