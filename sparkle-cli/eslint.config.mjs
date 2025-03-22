1c1bd345d2c6da191a7d076d3da38b2c1a8842cc
cream <837800910@qq.com>
2025-03-22 17:31:48 +0800

    简单配置eslint

--- sparkle-cli/eslint.config.mjs
+++ sparkle-cli/eslint.config.mjs
@@ -6,6 +6,7 @@ import pluginVue from "eslint-plugin-vue";
 
 
 export default defineConfig([
+    {ignores: ["dist*/*", "**/node_modules"]},
     {files: ["**/*.{js,mjs,cjs,ts,vue}"]},
     {files: ["**/*.{js,mjs,cjs,ts,vue}"], languageOptions: {globals: {...globals.browser, ...globals.node}}},
     {files: ["**/*.{js,mjs,cjs,ts,vue}"], plugins: {js}, extends: ["js/recommended"]},
@@ -14,9 +15,19 @@ export default defineConfig([
     {files: ["**/*.vue"], languageOptions: {parserOptions: {parser: tseslint.parser}}},
     {
         files: ["**/*.{js,mjs,cjs,ts,vue}"],
-        // 禁用 @typescript-eslint/no-explicit-any 规则，允许使用 any 类型
         rules: {
-            "@typescript-eslint/no-explicit-any": "off"
+            //----自定义规则----
+            quotes: ["error", "double"], // 使用双引号
+            semi: ["error", "always"], // 使用分号
+            "no-unused-vars": "off",
+            "@typescript-eslint/no-unused-vars": "off",
+            // 禁用 @typescript-eslint/no-explicit-any 规则，允许使用 any 类型
+            "@typescript-eslint/no-explicit-any": "off",
+            "vue/no-unused-vars": "off",
+            "linebreak-style": ["error", "unix"],
+            // 禁用 vue/require-v-for-key 规则
+            // todo 要确认v-for少了key具体有什么影响，这条配置后面可能需要去掉（视影响大小）
+            "vue/require-v-for-key": "off"
         }
     }
 ]);
