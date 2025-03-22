63fa183f6cbad4a1cc058c606ebb9e8eae1c0cf7
cream <837800910@qq.com>
2025-03-22 09:05:34 +0800

    功能栏图标

--- sparkle-cli/src/components/ToolBar.vue
+++ sparkle-cli/src/components/ToolBar.vue
@@ -1,25 +1,33 @@
 <script setup lang="ts">
+
 const tools = [
   {
     title: "music",
-    icon: ""
+    icon: "music.svg"
   },
   {
     title: "note",
-    icon: ""
+    icon: "note.svg"
+  },
+  {
+    title: "chat",
+    icon: "chat.svg"
   },
   {
     title: "setting",
-    icon: ""
+    icon: "setting.svg"
   }
 ]
-
+const computeIconPath = (item: any) => {
+  return new URL(`../assets/icon/${item.icon}`, import.meta.url).href;
+}
 </script>
 
 <template>
   <div class="all-item-container">
-    <div v-for="item in tools" class="tool-item" :class="[item.title==='setting'?'setting-item':'']">
-      {{ item.title }}
+    <div v-for="item in tools" :title="item.title" class="tool-item"
+         :class="[item.title==='setting'?'setting-item':'']">
+      <img class="item-icon" :src="computeIconPath(item)" :alt="item.title">
     </div>
   </div>
 </template>
@@ -27,6 +35,12 @@ const tools = [
 <style scoped>
 .tool-item {
   height: 32px;
+  transition: background-color 0.3s ease;
+  padding: 2px 0;
+}
+
+.tool-item:hover {
+  background-color: rgba(0, 0, 0, 0.2); /* 背景变深 */
 }
 
 .all-item-container {
@@ -38,5 +52,12 @@ const tools = [
 
 .setting-item {
   margin-top: auto;
+}
+
+.item-icon {
+  display: block;
+  width: 25px;
+  height: 25px;
+  margin: 0 auto;
 }
 </style>
