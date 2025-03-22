8092fecd02a5058eb51a2fbccd9a2c67bfce5ff1
cream <837800910@qq.com>
2025-03-22 23:00:37 +0800

    ...

--- sparkle-cli/eslint.config.mjs
+++ sparkle-cli/eslint.config.mjs
@@ -6,7 +6,7 @@ import pluginVue from "eslint-plugin-vue";
 import eslintConfigPrettier from "eslint-config-prettier/flat";
 
 export default defineConfig([
-  { ignores: ["dist*/*", "**/node_modules", "build/*"] },
+  { ignores: ["dist*/*", "**/node_modules", "build/*", "release/*"] },
   { files: ["**/*.{js,mjs,cjs,ts,vue}"] },
   {
     files: ["**/*.{js,mjs,cjs,ts,vue}"],
