80a9b2791f65660abd676a249550efd64aa3c666
cream <837800910@qq.com>
2025-03-23 22:57:06 +0800

    [back] 整合fastJson,并移除jackson

--- sparkle-server/src/main/java/com/cream/sparkle/config/mvc/AppWebMvcConfigurer.java
+++ sparkle-server/src/main/java/com/cream/sparkle/config/mvc/AppWebMvcConfigurer.java
@@ -1,14 +1,39 @@
 package com.cream.sparkle.config.mvc;
 
+import com.alibaba.fastjson2.JSONReader;
+import com.alibaba.fastjson2.JSONWriter;
+import com.alibaba.fastjson2.support.config.FastJsonConfig;
+import com.alibaba.fastjson2.support.spring6.http.converter.FastJsonHttpMessageConverter;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.http.MediaType;
+import org.springframework.http.converter.HttpMessageConverter;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+import java.nio.charset.StandardCharsets;
+import java.util.Collections;
+import java.util.List;
 
 @Slf4j
 @Configuration
 public class AppWebMvcConfigurer implements WebMvcConfigurer {
 
+    @Override
+    public void configureMessageConverters(List<HttpMessageConverter<?>> converters) {
+        FastJsonHttpMessageConverter converter = new FastJsonHttpMessageConverter();
+        //自定义配置...
+        FastJsonConfig config = new FastJsonConfig();
+        config.setDateFormat("yyyy-MM-dd HH:mm:ss");
+        config.setReaderFeatures(JSONReader.Feature.FieldBased, JSONReader.Feature.SupportArrayToBean);
+        // ※最后一个是配置long类型转换为string，避免前端丢失精度
+        config.setWriterFeatures(JSONWriter.Feature.WriteMapNullValue, JSONWriter.Feature.PrettyFormat, JSONWriter.Feature.WriteLongAsString);
+
+        converter.setFastJsonConfig(config);
+        converter.setDefaultCharset(StandardCharsets.UTF_8);
+        converter.setSupportedMediaTypes(Collections.singletonList(MediaType.APPLICATION_JSON));
+        converters.addFirst(converter);
+    }
 
     /**
      * 跨域配置
