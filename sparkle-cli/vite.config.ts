1598ff4d58c81d61c2086b7db567181a41309117
cream <837800910@qq.com>
2025-03-25 00:55:34 +0800

    [front] sourcemap配置

--- sparkle-cli/vite.config.ts
+++ sparkle-cli/vite.config.ts
@@ -3,34 +3,40 @@ import vue from "@vitejs/plugin-vue";
 import electron from "vite-plugin-electron/simple";
 import * as path from "node:path";
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [
-    vue(),
-    electron({
-      main: {
-        entry: "electron/main.ts",
-        vite: {
-          build: {
-            outDir: "build/main"
+export default defineConfig(({ mode, command }) => {
+  const sourcemap = mode === "development" && command === "serve";
+  return {
+    plugins: [
+      vue(),
+      electron({
+        main: {
+          entry: "electron/main.ts",
+          vite: {
+            build: {
+              outDir: "build/main",
+              sourcemap
+            }
+          }
+        },
+        preload: {
+          input: "electron/preload/preload.ts",
+          vite: {
+            build: {
+              outDir: "build/main",
+              sourcemap
+            }
           }
         }
-      },
-      preload: {
-        input: "electron/preload/preload.ts",
-        vite: {
-          build: {
-            outDir: "build/main"
-          }
-        }
+      })
+    ],
+    resolve: {
+      alias: {
+        "@": path.resolve(__dirname, "src")
       }
-    })
-  ],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "src")
+    },
+    build: {
+      outDir: "build/renderer",
+      sourcemap
     }
-  },
-  build: {
-    outDir: "build/renderer"
-  }
+  };
 });
