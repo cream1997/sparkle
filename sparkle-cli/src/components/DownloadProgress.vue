473ccb6b057a403086e9d5e595974d3194bb3b41
cream <837800910@qq.com>
2025-03-25 16:34:16 +0800

    [front] 下载进度条组件

--- sparkle-cli/src/components/DownloadProgress.vue
+++ sparkle-cli/src/components/DownloadProgress.vue
@@ -1,14 +1,46 @@
 <script setup lang="ts">
+import { computed } from "vue";
+
 interface Props {
   totalSize: number;
   downloadSize: number;
 }
 
-defineProps<Props>();
+const props = defineProps<Props>();
+
+const percent = computed(() =>
+  ((props.downloadSize / props.totalSize) * 100).toFixed(1)
+);
+
+const jinHaoProgress = computed(() => {
+  const count = Math.floor(parseInt(percent.value) / 10);
+  return "#".repeat(count);
+});
 </script>
 
 <template>
-  <div>总大小{{ totalSize }}， 当前进度{{ downloadSize }}</div>
+  <div class="container-DownloadProgress">
+    <div v-if="parseInt(percent) < 100">
+      下载中
+      <span class="progress-bar-container-DownloadProgress">{{
+        jinHaoProgress
+      }}</span>
+      {{ percent }}%
+    </div>
+    <div v-else>下载完成, 启动安装...</div>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.container-DownloadProgress {
+  width: 165px;
+  border-right: 1px solid lightgray;
+
+  .progress-bar-container-DownloadProgress {
+    display: inline-block;
+    width: 85px;
+    border-left: 1px solid lightgray;
+    border-right: 1px solid lightgray;
+  }
+}
+</style>
