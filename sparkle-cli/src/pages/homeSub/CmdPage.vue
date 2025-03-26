e86620675d445baec1dd52916b91ed9c4477bc86
cream <837800910@qq.com>
2025-03-26 18:05:02 +0800

    [front] 引入xterm(npm install @xterm/xterm)

--- sparkle-cli/src/pages/homeSub/CmdPage.vue
+++ sparkle-cli/src/pages/homeSub/CmdPage.vue
@@ -1,7 +1,27 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import "@xterm/xterm/css/xterm.css";
+import { onMounted, onUnmounted, ref } from "vue";
+import { Terminal } from "@xterm/xterm";
+
+const terminalRef = ref();
+
+let term: Terminal | null = null;
+onMounted(() => {
+  term = new Terminal();
+  term.open(terminalRef.value);
+  term.write("hello world");
+});
+onUnmounted(() => {
+  if (term) {
+    term.dispose();
+  }
+});
+</script>
 
 <template>
-  <div class="container-cmdPage"></div>
+  <div class="container-cmdPage">
+    <div ref="terminalRef"></div>
+  </div>
 </template>
 
 <style scoped>
