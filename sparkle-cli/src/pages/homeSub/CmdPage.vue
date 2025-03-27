da989ef63d4c9b63421e80d861586b6b1f72aa1e
cream <837800910@qq.com>
2025-03-27 13:30:43 +0800

    [front] 终端窗口自适应大小调整，填补高度空缺

--- sparkle-cli/src/pages/homeSub/CmdPage.vue
+++ sparkle-cli/src/pages/homeSub/CmdPage.vue
@@ -1,76 +1,84 @@
 <script setup lang="ts">
 import "@xterm/xterm/css/xterm.css";
-import { onMounted, onUnmounted, ref } from "vue";
+import { onMounted, onUnmounted, reactive, ref, toRaw } from "vue";
 import { Terminal } from "@xterm/xterm";
 import IpcChannels from "../../../common/IpcChannels.ts";
 import { FitAddon } from "@xterm/addon-fit";
 
-const host = ref("192.168.33.100");
-const port = ref(22);
-const username = ref("test");
-const password = ref("123456");
-const terminalRef = ref();
-let term: Terminal;
+const linkInfo = reactive({
+  host: "192.168.33.100",
+  port: 22,
+  username: "test",
+  password: "123456"
+});
+
+const terminalContainerRef = ref();
+let terminal: Terminal;
 onMounted(() => {
-  term = new Terminal({
+  terminal = new Terminal({
     theme: { background: "#1a1a1a", foreground: "#ffffff" },
     fontSize: 16,
     cursorBlink: true,
     cursorStyle: "bar"
   });
   const fitAddon = new FitAddon();
-  term.loadAddon(fitAddon);
-  term.open(terminalRef.value);
-  fitAddon.fit();
-  const resizeObserver = new ResizeObserver(() => {
+  terminal.loadAddon(fitAddon);
+  terminal.open(terminalContainerRef.value);
+
+  function adjustSize() {
     fitAddon.fit();
+    // 上面只设置了行列数，但是高度还可能有空隙
+    const el = terminalContainerRef.value.querySelector(".xterm-screen");
+    if (el) {
+      el.style.height = terminalContainerRef.value.offsetHeight + "px";
+    }
+  }
+
+  adjustSize();
+  const resizeObserver = new ResizeObserver(() => {
+    adjustSize();
   });
-  resizeObserver.observe(terminalRef.value);
+  resizeObserver.observe(terminalContainerRef.value);
   onUnmounted(() => {
     resizeObserver.disconnect();
   });
   let currentLine = "";
-  term.onData((data) => {
+  terminal.onData((data) => {
     if (data === "\x7f" || data === "\x08") {
       // 退格键(Backspace)
       if (currentLine.length > 0) {
         currentLine = currentLine.slice(0, -1);
         // 退格 + 空格 + 退格（覆盖前字符）
-        term.write("\b \b");
+        terminal.write("\b \b");
       }
     } else if (data === "\r") {
       // 处理回车的情况
-      term.write("\r\n");
+      terminal.write("\r\n");
       // 发送当前行
       sendSshMsg(currentLine);
       currentLine = "";
     } else {
       // 其他字符
       currentLine += data;
-      term.write(data);
+      terminal.write(data);
     }
   });
 
   window.ipc.on(IpcChannels.SshReceiveData, (event, chunk) => {
-    term.write(chunk);
+    terminal.write(chunk);
   });
 });
 
 onUnmounted(() => {
   logout();
-  if (term) {
-    term.dispose();
+  if (terminal) {
+    terminal.dispose();
   }
   window.ipc.removeAllListeners(IpcChannels.SshReceiveData);
 });
 
 const login = () => {
-  window.ipc.send(IpcChannels.SshLogin, {
-    host: host.value,
-    port: port.value,
-    username: username.value,
-    password: password.value
-  });
+  window.ipc.send(IpcChannels.SshLogin, toRaw(linkInfo));
 };
 const logout = () => {
   window.ipc.send(IpcChannels.SshLogout);
@@ -84,22 +92,22 @@ const sendSshMsg = (msg: string) => {
 <template>
   <div class="container">
     <div class="menu">
-      <input disabled placeholder="服务器ip" v-model="host" />
+      <input disabled placeholder="服务器ip" v-model="linkInfo.host" />
       <input
         disabled
         placeholder="ssh端口"
-        v-model.number="port"
+        v-model.number="linkInfo.port"
         type="number"
       />
       用户名
-      <input disabled v-model="username" />
+      <input disabled v-model="linkInfo.username" />
       密码
-      <input v-model="password" type="password" />
+      <input v-model="linkInfo.password" type="password" />
       <button @click="login">登录</button>
       <button @click="logout">退出</button>
     </div>
     <div class="content-area">
-      <div class="terminal" ref="terminalRef"></div>
+      <div class="terminal" ref="terminalContainerRef"></div>
       <div class="terminal-bottom-info-panel">终端信息栏</div>
     </div>
   </div>
