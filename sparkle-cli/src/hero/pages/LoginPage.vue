d2c39b82cd8b090ba34644243e5a7dcde3ad5656
cream <837800910@qq.com>
2025-04-13 20:27:11 +0800

    [Hero-front] 登录页面简单样式

--- sparkle-cli/src/hero/pages/LoginPage.vue
+++ sparkle-cli/src/hero/pages/LoginPage.vue
@@ -2,9 +2,11 @@
 import { ref } from "vue";
 import { post } from "@/net/AxiosCfg.ts";
 import HttpApiOfHero from "@/hero/net/HttpApiOfHero.ts";
+import swordPng from "@/assets/png/sword.png";
 
 const username = ref("123456");
 const password = ref(123456);
+const nickName = ref("小黑");
 
 function login(event: MouseEvent) {
   event.preventDefault();
@@ -20,18 +22,58 @@ function register(event: MouseEvent) {
 </script>
 
 <template>
-  <form>
-    <div>
-      <label for="username">账号</label>
-      <input id="username" v-model.number="username" />
-    </div>
-    <div>
-      <label for="password">密码</label>
-      <input id="password" type="password" v-model="password" />
-    </div>
-    <button @click="login">登录</button>
-    <button @click="register">注册</button>
-  </form>
+  <div class="ctn-loginPage">
+    <h1 class="title">
+      <img class="sword-icon" :src="swordPng" alt="勇士之剑" />
+      Hero
+    </h1>
+    <form>
+      <div class="row">
+        <label for="username">账号</label>
+        <input id="username" v-model.number="username" />
+      </div>
+      <div class="row">
+        <label for="password">密码</label>
+        <input id="password" type="password" v-model="password" />
+      </div>
+      <div class="row">
+        <label for="nickname">昵称</label>
+        <input id="nickname" v-model="nickName" />
+      </div>
+      <div class="row btn-row">
+        <button @click="login">登录</button>
+        <button @click="register">注册</button>
+      </div>
+    </form>
+  </div>
 </template>
 
-<style scoped></style>
+<style scoped>
+.ctn-loginPage {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  padding-top: 36px;
+
+  .title {
+    .sword-icon {
+      height: 25px;
+    }
+  }
+
+  .row {
+    height: 30px;
+
+    > * {
+      line-height: 20px;
+    }
+  }
+
+  .btn-row {
+    display: flex;
+    justify-content: center;
+  }
+}
+</style>
