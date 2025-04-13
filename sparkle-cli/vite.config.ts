f4e900dd89d58787c24dfe599c9adf9d9e588ec5
cream <837800910@qq.com>
2025-04-14 01:02:36 +0800

    [front] 箭头函数单个参数不用括号、主进程构建解析@路径

--- sparkle-cli/vite.config.ts
+++ sparkle-cli/vite.config.ts
@@ -17,13 +17,22 @@ export default defineConfig(({ mode, command }) => {
             build: {
               outDir: "build/main",
               sourcemap: isDev
+            },
+            resolve: {
+              /**
+               * 配置这个是因为主进程有一处引用了渲染进程的代码，就是axios的配置是一份共用的
+               * 而axiosCfg的配置中导入用了@
+               */
+              alias: {
+                "@": path.resolve(__dirname, "src")
+              }
             }
           },
           /**
            * 启动前做些事情...
            * 主要就是配置渲染进程调试端口
            */
-          onstart: (args) => {
+          onstart: args => {
             // 启动electron 应用
             if (isDev) {
               args.startup([
