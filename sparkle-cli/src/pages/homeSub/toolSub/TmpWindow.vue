6ff3a5c6bf2ca2314878e93bc008291d6db03d90
cream <837800910@qq.com>
2025-04-16 21:05:33 +0800

    ...

--- sparkle-cli/src/pages/homeSub/toolSub/TmpWindow.vue
+++ sparkle-cli/src/pages/homeSub/toolSub/TmpWindow.vue
@@ -1,7 +1,22 @@
-<script setup lang="ts"></script>
+<script setup lang="ts">
+import { Application } from "pixi.js";
+import { onMounted, ref } from "vue";
+import "pixi.js/unsafe-eval";
+
+const ctnRef = ref();
+
+onMounted(async () => {
+  const app = new Application();
+  await app.init({
+    background: "#1099bb",
+    resizeTo: ctnRef.value
+  });
+  ctnRef.value.appendChild(app.canvas);
+});
+</script>
 
 <template>
-  <div class="container"></div>
+  <div class="container" ref="ctnRef"></div>
 </template>
 
 <style scoped>
