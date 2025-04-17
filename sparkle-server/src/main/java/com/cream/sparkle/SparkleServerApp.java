0ff626d2251bf0c8723ca4308f2388ecd90df538
cream <837800910@qq.com>
2025-04-17 20:52:10 +0800

    禁用springSecurity默认的UserDetailsServiceAutoConfiguration

--- sparkle-server/src/main/java/com/cream/sparkle/SparkleServerApp.java
+++ sparkle-server/src/main/java/com/cream/sparkle/SparkleServerApp.java
@@ -3,9 +3,10 @@ package com.cream.sparkle;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration;
 
 @MapperScan({"com.cream.sparkle.mapper", "com.cream.sparkle.z.hero.mapper"})
-@SpringBootApplication
+@SpringBootApplication(exclude = {UserDetailsServiceAutoConfiguration.class})
 public class SparkleServerApp {
 
     public static void main(String[] args) {
