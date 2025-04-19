3c368362d664f8d9e0efb470f73698ff3b666262
cream <837800910@qq.com>
2025-04-20 07:22:03 +0800

    [hero-front] 修复缓存组件实例后引入的bug

--- sparkle-cli/src/hooks/useWatchTokenHook.ts
+++ sparkle-cli/src/hooks/useWatchTokenHook.ts
@@ -1,4 +1,4 @@
-import { onUnmounted, watchEffect } from "vue";
+import { onActivated, onDeactivated, watchEffect, type WatchHandle } from "vue";
 import Tip from "@/tools/Tip.ts";
 import { HeroPagePath } from "@/router/router.ts";
 import useAccountStore from "@/store/useAccountStore.ts";
@@ -9,21 +9,25 @@ const accountStore = useAccountStore();
 export default function useWatchTokenHook() {
   const router = useRouter();
   const route = useRoute();
-  const stopTokenWatch = watchEffect(() => {
-    const token = accountStore.token;
-    if (!token) {
-      Tip.err("token为空，请先登录", 800);
-      const oldPath = route.path;
-      setTimeout(() => {
-        if (route.path === oldPath) {
-          // 避免已经手动点击跳转到其他页面比如Music
-          router.push(HeroPagePath.LoginGame);
-        }
-      }, 900);
-    }
+
+  let stopTokenWatch: WatchHandle;
+  onActivated(() => {
+    stopTokenWatch = watchEffect(() => {
+      const token = accountStore.token;
+      if (!token) {
+        Tip.err("token为空，请先登录", 800);
+        const oldPath = route.path;
+        setTimeout(() => {
+          if (route.path === oldPath) {
+            // 避免已经手动点击跳转到其他页面比如Music
+            router.push(HeroPagePath.LoginGame);
+          }
+        }, 900);
+      }
+    });
   });
 
-  onUnmounted(() => {
+  onDeactivated(() => {
     stopTokenWatch();
   });
 }
