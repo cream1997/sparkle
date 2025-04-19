f49a806f2af6a86a53829e97fc12a4e046984470
cream <837800910@qq.com>
2025-04-20 04:28:09 +0800

    [hero] 修复客户端重复登录

--- sparkle-cli/electron/ipc/listeners/listenHeroChannels.ts
+++ sparkle-cli/electron/ipc/listeners/listenHeroChannels.ts
@@ -3,11 +3,15 @@ import { IpcChannelsOfHero } from "../../../common/IpcChannels.ts";
 import GameWebSocket from "../../net/HeroWebSocket.ts";
 import type { WsConnectResult } from "../../../common/types/WsConnectResult.ts";
 
+let gameWebSocket: GameWebSocket | null = null;
 export default function listenHeroChannels() {
   ipcMain.handle(
     IpcChannelsOfHero.WsConnect,
     async (_event, { ip, socketPort, token }) => {
-      const gameWebSocket = new GameWebSocket(ip, socketPort, token, msg => {});
+      if (gameWebSocket) {
+        gameWebSocket.close();
+      }
+      gameWebSocket = new GameWebSocket(ip, socketPort, token, msg => {});
       const connectResult: WsConnectResult = { success: false, err: null };
       try {
         await gameWebSocket.connect();
