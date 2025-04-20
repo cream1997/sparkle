55dee53cf17a69f258c23efc97fbe23cc116a68a
cream <837800910@qq.com>
2025-04-21 04:48:20 +0800

    [hero-front] 重构代码位置

--- sparkle-cli/src/z/hero/HeroPage.vue
+++ sparkle-cli/src/z/hero/HeroPage.vue
@@ -1,5 +1,34 @@
 <script setup lang="ts">
-import { HeroSubPageViewName } from "@/router/RouterConst";
+import { BasePagePath, HeroSubPageViewName } from "@/router/RouterConst";
+import useRouteInfoStore from "@/store/useRouteInfoStore.ts";
+import { useRouter } from "vue-router";
+
+const router = useRouter();
+const routeInfoStore = useRouteInfoStore();
+
+/**
+ * 定义路由守卫用来恢复子路由路径
+ */
+router.beforeEach((to, from, next) => {
+  let skipPath = to.path;
+  if (
+    !from.path.startsWith(BasePagePath.Hero) &&
+    to.path.startsWith(BasePagePath.Hero)
+  ) {
+    skipPath = routeInfoStore.heroPageCurrentPath
+      ? routeInfoStore.heroPageCurrentPath
+      : skipPath;
+  }
+  if (skipPath.startsWith(BasePagePath.Hero)) {
+    routeInfoStore.heroPageCurrentPath = skipPath;
+  }
+  // 不能直接简化写成next(skipPath),因为next函数传值就会导致再执行router.beforeEach导致无限循环
+  if (skipPath === to.path) {
+    next();
+  } else {
+    next(skipPath);
+  }
+});
 </script>
 
 <template>
